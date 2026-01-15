7ca8712a094caa4f08fd6eb2a562dc15cc1de366
Kush Vardhan <kushvardhan39797@gmail.com>
2026-01-15 21:18:18 +0530

    Feat: Added Get, delete, update API

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -13,7 +13,7 @@ function pingProblemController(req,res){
 async function addProblem(req,res,next){
     try{
         const newProblem = await problemService.createProblem(req.body);
-        
+        console.log("FROM Controller:",newProblem);
         return res.status(StatusCodes.CREATED).json({
             success:true,
             message:"Successfully created a new problem",
@@ -26,41 +26,83 @@ async function addProblem(req,res,next){
     }
 }
 
-function getProblem(req,res,next){
+async function getProblem(req,res,next){
     try{
-        // Nothing is implemented Yet
-        throw new NotImplementedError("getProblem");
+        const { id } = req.params;
+
+        const problem = await problemService.getProblem(id);
+        console.log("FROM Controller:",problem);
+
+        return res.status(StatusCodes.CREATED).json({
+            success:true,
+            message:"Successfully fetched a problem",
+            error:{},
+            data : problem,
+        }) 
+
     }catch(err){
         console.log("Error: ", err);
         next(err);
     }}
 
-function getProblems(req,res,next){
+async function getProblems(req,res,next){
     try{
-        // Nothing is implemented Yet
-        throw new NotImplementedError("getProblems");
+        const problems = await problemService.getProblems();
+        console.log("FROM Controller:",problems);
+
+        return res.status(StatusCodes.CREATED).json({
+            success:true,
+            message:"Successfully fetched all problems",
+            error:{},
+            data : problems,
+        }) 
+
     }catch(err){
         console.log("Error: ", err);
         next(err);
     }}
 
-function updateProblem(req,res,next){
-    try{
-        // Nothing is implemented Yet
-        throw new NotImplementedError("updateProblem");
-    }catch(err){
-        console.log("Error: ", err);
-        next(err);
-    }}
+async function updateProblem(req, res, next) {
+    try {
+        const { id } = req.params;
 
-function deleteProblem(req,res,next){
-    try{
-        // Nothing is implemented Yet
-        throw new NotImplementedError("deleteProblem");
-    }catch(err){
+        if (!id) {
+            throw new BadRequest("id", "Problem ID is required");
+        }
+
+        const updatedProblem = await problemService.updateProblem(id, req.body);
+        console.log("FROM Controller:",updatedProblem);
+
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Successfully updated the problem",
+            error: {},
+            data: updatedProblem,
+        });
+
+    } catch (err) {
         console.log("Error: ", err);
         next(err);
-    }}
+    }
+}
+
+
+async function deleteProblem(req, res, next) {
+    try {
+        const problem = await problemService.deleteProblem(req.params.id);
+
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Successfully deleted a problem",
+            error: {},
+            data: problem,
+        });
+    } catch (err) {
+        console.log("Error: ", err);
+        next(err);
+    }
+}
+
 
 module.exports={
     pingProblemController,
--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -9,13 +9,66 @@ class ProblemRepository{
                 description:problemData.description,
                 testCases : (problemData.testCases) ? problemData.testCases : []
             });
-
+            console.log("FROM REPO:",problem);
             return problem;
         }catch(err){
             console.log(err);
             throw err;
         }
     }
+
+    async getProblem(problemId){
+        try{
+            const problem = await Problem.findById(problemId);
+            console.log("FROM REPO:",problem);
+            return problem;
+        }catch(err){
+            console.log(err);
+            throw err;
+        }
+    }
+
+    async getProblems(){
+        try{
+            const problems = await Problem.find();
+            console.log("FROM REPO:",problems);
+            return problems;
+        }catch(err){
+            console.log(err);
+            throw err;
+        }
+    }
+
+    async deleteProblem(problemId) {
+    try {
+        const problem = await Problem.findByIdAndDelete(problemId);
+        console.log("FROM REPO:",problem);
+        return problem;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
+
+    async updateProblem(problemId, updateData) {
+    try {
+        const problem = await Problem.findByIdAndUpdate(
+            problemId,
+            updateData,
+            {
+                new: true,          // return updated doc
+                runValidators: true
+            }
+        );
+        console.log("FROM REPO:",problem);
+        return problem;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
 
 }
 
